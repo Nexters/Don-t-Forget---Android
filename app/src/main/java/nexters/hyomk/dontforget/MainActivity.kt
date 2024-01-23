1490935db3697e73e3ab7bd584b1c8f1377b5a88
Hyomk <qet557@gmail.com>
2024-01-23 16:27:27 +0900

    feat: font 추가 #7

--- app/src/main/java/nexters/hyomk/dontforget/MainActivity.kt
+++ app/src/main/java/nexters/hyomk/dontforget/MainActivity.kt
@@ -10,19 +10,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import nexters.hyomk.dontforget.ui.theme.Dont_forgetTheme
+import dagger.hilt.android.AndroidEntryPoint
+import nexters.hyomk.dontforget.ui.theme.DontForgetTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            Dont_forgetTheme {
+            DontForgetTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    Greeting("Android")
                 }
             }
         }
@@ -40,7 +42,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Dont_forgetTheme {
+    DontForgetTheme {
         Greeting("Android")
     }
 }
