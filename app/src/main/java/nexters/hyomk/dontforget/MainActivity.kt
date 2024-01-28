75cb1f1a311cdab3a1007259a764f06c34afb274
Hyomk <qet557@gmail.com>
2024-01-28 16:54:11 +0900

    feat: Acitionbar 비활성화 #12

--- app/src/main/java/nexters/hyomk/dontforget/MainActivity.kt
+++ app/src/main/java/nexters/hyomk/dontforget/MainActivity.kt
@@ -1,6 +1,7 @@
 package nexters.hyomk.dontforget
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -29,6 +30,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+        )
 
         setContent {
             DontForgetTheme {
