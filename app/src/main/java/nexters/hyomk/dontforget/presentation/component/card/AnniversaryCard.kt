8035fec80b5a78d41cb60315e1484910da9291e8
Hyomk <qet557@gmail.com>
2024-02-26 23:55:25 +0900

    fix: card text overflow 처리

--- app/src/main/java/nexters/hyomk/dontforget/presentation/component/card/AnniversaryCard.kt
+++ app/src/main/java/nexters/hyomk/dontforget/presentation/component/card/AnniversaryCard.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import nexters.hyomk.domain.model.AnniversaryCardType
@@ -86,6 +87,7 @@ fun AnniversaryCard(
             Text(
                 text = title,
                 maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
                 style = MaterialTheme.typography.headlineSmall.copy(color = properties.titleColor),
             )
             Text(
